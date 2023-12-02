f01c23f9291da2a6fe608761b837d333ae458ecd
Martina Randulová <extRandulova@dr-max.global>
2023-12-02 17:17:00 +0100

    minor refactor

--- day2/day2_solution.js
+++ day2/day2_solution.js
@@ -1,9 +1,10 @@
 const { readFileSync } = require('fs');
 
 const lines = readFileSync('./input.txt', 'utf-8');
-let sum = 0;
 
 // 1. part - 12 red cubes, 13 green cubes, and 14 blue cubes
+let sum = 0;
+
 lines.split('\n').forEach(line => {
   let [gameId, games] = line.split(':');
   gameId = gameId.slice(5);
@@ -23,27 +24,30 @@ lines.split('\n').forEach(line => {
   }
 })
 
-//2nd part - minimum necessary color counts
-// lines.split('\n').forEach(line => {
-//   let [gameId, games] = line.split(':');
-//   gameId = gameId.slice(5);
-//   let gamesArr = games.split(';');
-//   let gameObj = {
-//     blue: 0,
-//     red: 0,
-//     green: 0
-//   }
-//   gamesArr.forEach(game => {
-//     let counts = game.split(',');
-//     counts.forEach(count => {
-//       const color = count.split(' ')[2];
-//       const colorCount = Number(count.split(' ')[1]);
-//       if (colorCount > Number(gameObj[color])) {
-//         gameObj[color] = colorCount
-//       }
-//     })
-//   })
-//   sum += gameObj.blue * gameObj.red * gameObj.green
-// })
+console.log(`solution to the first part is ${sum}`)
 
-console.log(sum)
+//2nd part - minimum necessary color counts
+let sum2 = 0;
+lines.split('\n').forEach(line => {
+  let [gameId, games] = line.split(':');
+  gameId = gameId.slice(5);
+  let gamesArr = games.split(';');
+  let gameObj = {
+    blue: 0,
+    red: 0,
+    green: 0
+  }
+  gamesArr.forEach(game => {
+    let counts = game.split(',');
+    counts.forEach(count => {
+      const color = count.split(' ')[2];
+      const colorCount = Number(count.split(' ')[1]);
+      if (colorCount > Number(gameObj[color])) {
+        gameObj[color] = colorCount
+      }
+    })
+  })
+  sum2 += gameObj.blue * gameObj.red * gameObj.green
+})
+
+console.log(`solution to the second part is ${sum2}`)
