2b6dd219fcbd1c7da5db4a2b72d44e472ab64d74
Martina Randulová <extRandulova@dr-max.global>
2023-12-04 16:27:51 +0100

    second part solution done

--- day3/day3_solution.js
+++ day3/day3_solution.js
@@ -3,7 +3,7 @@ const { readFileSync } = require('fs');
 const lines = readFileSync('./input.txt', 'utf-8');
 
 let sum = 0;
-let sumOfAll = 0;
+let sumGear = 0;
 let grid = Array.from(Array(142).fill('.'), () => new Array(142).fill('.'))
 
 const SuspiciousPartEngine = class {
@@ -15,14 +15,27 @@ const SuspiciousPartEngine = class {
   }
 }
 
+const SuspiciousStars = class {
+  constructor(lineNumber, position, number) {
+    this.id = String(lineNumber) + '-' + String(position)
+    this.adjacent = [];
+    this.addAdjacentNumber(number)
+  }
+
+  addAdjacentNumber = (number) => {
+    this.adjacent.push(number)
+  }
+}
+
 const suspiciousPartEngineArr = [];
+const suspiciousStarsArr = [];
 
 const linesArr = lines.split('\n');
 
 linesArr.forEach((line, lineNumber) => {
-  const regexp = /\d\d?\d?/g;
+  const regexpNumbers = /\d\d?\d?/g;
   let match;
-  while ((match = regexp.exec(line)) !== null) {
+  while ((match = regexpNumbers.exec(line)) !== null) {
     suspiciousPartEngineArr.push(new SuspiciousPartEngine(match[0], lineNumber + 1, match.index + 1));
   }
 
@@ -35,6 +48,14 @@ const checkSymbol = (object, y, x) => {
   const symbol = grid[object.line + y][object.positionxstart + x];
   if (symbol !== '.' && isNaN(symbol)) {
     object.partNumber = true;
+    if (symbol === '*') {
+      const starAlreadySeen = suspiciousStarsArr.filter(star => star.id === String(object.line + y) + '-' + String(object.positionxstart + x))
+      if (starAlreadySeen[0]) {
+        starAlreadySeen[0].addAdjacentNumber(object.number);
+      } else {
+        suspiciousStarsArr.push(new SuspiciousStars(object.line + y, object.positionxstart + x, object.number));
+      }
+    }
   }
 }
 
@@ -47,28 +68,37 @@ for (let partNumberIndex = 0; partNumberIndex < suspiciousPartEngineArr.length; 
   checkSymbol(numberObject, 1, -1);
   checkSymbol(numberObject, 1, 0);
   checkSymbol(numberObject, 1, 1);
-
+  if (Number(numberObject.number) > 9 && numberObject.partNumber === false) {
+    checkSymbol(numberObject, -1, 2);
+    checkSymbol(numberObject, 1, 2);
+  }
   if ((Number(numberObject.number) <= 9) && numberObject.partNumber === false) {
     checkSymbol(numberObject, 0, 1);
-  } else if ((Number(numberObject.number) <= 99) && numberObject.partNumber === false) {
-    checkSymbol(numberObject, -1, 2);
+  }
+  if ((Number(numberObject.number) <= 99) && numberObject.partNumber === false) {
     checkSymbol(numberObject, 0, 2);
-    checkSymbol(numberObject, 1, 2);
-  } else if (Number(numberObject.number) > 99 && numberObject.partNumber === false) {
-    checkSymbol(numberObject, -1, 2);
+  }
+  if (Number(numberObject.number) > 99 && numberObject.partNumber === false) {
     checkSymbol(numberObject, -1, 3);
-    checkSymbol(numberObject, 1, 2);
     checkSymbol(numberObject, 1, 3);
     checkSymbol(numberObject, 0, 3);
   }
 
   if (numberObject.partNumber) {
     sum += Number(numberObject.number);
-  } else {
-    sumOfAll -= Number(numberObject.number);
   }
 }
 
 console.log(`solution to the first part is ${sum}`)
 
 //2nd part
+console.log(suspiciousStarsArr)
+suspiciousStarsArr.forEach(starObj => {
+  if (starObj.adjacent.length === 2) {
+    sumGear += Number(starObj.adjacent[0]) * Number(starObj.adjacent[1]);
+  }
+})
+console.log(`solution to the first part is ${sumGear}`)
+//15627729 is too low
+//84579863 is too low
+//84584891
